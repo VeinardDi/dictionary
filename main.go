c18fe6dfe0dcfb1f55fc77cb663c3dd7584a7c5d
VeinardDi <44543585+VeinardDi@users.noreply.github.com>
2018-11-06 10:05:43 +0300

    Fixed bug with adding to array.
    Fixed bug with adding to array. The array was initialized differently.

--- main.go
+++ main.go
@@ -30,21 +30,18 @@ func main() {
 	s := string(array)
 	re := regexp.MustCompile("[a-яА-ЯёЁ]+")
 	newSTRINGS := re.FindAllString(s, -1)
-	fmt.Println(newSTRINGS)
 
 	f, err := ioutil.ReadFile("dictionary.json")
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 		return
 	}
-	fmt.Println(f)
 
 	var dictionaryMAP map[string]string
-	inputARRAY := make([]string, size.Size())
+	var inputARRAY []string
 
 	err = json.Unmarshal(f, &dictionaryMAP)
 	fmt.Println("ERROR parsing", err)
-	fmt.Println(dictionaryMAP)
 
 	for i := 0; i<10; i++ {
 		if k, ok := dictionaryMAP[newSTRINGS[i]]; ok {
@@ -52,11 +49,7 @@ func main() {
 		}
 	}
 
-	//var reg = regexp.MustCompile(`[a-zA-Z]+`)
 	outputSTRINGS := strings.Join(inputARRAY, " ")
-	//s := reg.FindAllString(str3, -1)
-	//st := strings.Join(s, " ")
-
 	output, err:= os.Create("outout.txt")
 	if err != nil {
 		return
